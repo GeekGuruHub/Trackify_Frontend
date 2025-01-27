bd9ec341741a2e1aa621620a01b5f55a642d0f9f
GeekGuruHub <geekguruhub101@gmail.com>
2025-01-27 09:44:24 +0200

    changes to the dashbaord

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,32 +2,36 @@ import { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import ExpenseForm from "../Components/ExpenseForm";
 
-function Dashboard(){
+function Dashboard() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("userName");
+    localStorage.removeItem("authToken");
+    navigate("/form"); // Redirect user to the login page
+  };
 
-    const handleLogout = () => {
-        localStorage.removeItem('userName');
-        localStorage.removeItem('authToken');
-        navigate('/form'); // Redirect user to the login page
-    };
-    
-    const userName = localStorage.getItem("userName");
+  const userName = localStorage.getItem("userName");
 
-    return(
-        <Fragment>
-            <div>Welcome,{userName}!</div>
-            
-            <br/>
-            <ExpenseForm/>
+  return (
+    <Fragment>
+      <div className="text-2xl font-semibold">Welcome, {userName}!</div>
 
-            <br/>
+      <br />
+      <ExpenseForm />
 
-            <div>
-                <button onClick={handleLogout}>Log Out</button>
-            </div>
-        </Fragment>
-    );
+      <br />
+
+      <div>
+        <button
+          className="w-55 p-2 bg-gray-600 text-black rounded-xl mt-3 hover:bg-green-600 hover:text-white text-sm md:text-base pt-4 pb-4"
+          onClick={handleLogout}
+        >
+          Log Out
+        </button>
+      </div>
+    </Fragment>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
